3cd788fbb09de594b8d442fb53fa71608f7c0275
Riad Gozim <rgozim1@gmail.com>
2018-03-08 16:08:32 +0000

    Renamed outputFile to outFile

--- dslplugin/src/main/groovy/dslplugin/DslOperation.groovy
+++ dslplugin/src/main/groovy/dslplugin/DslOperation.groovy
@@ -11,7 +11,7 @@ class DslOperation {
 
     File outputPath
 
-    File outputFile
+    File outFile
 
     Closure formatOutput
 
--- dslplugin/src/main/groovy/dslplugin/DslPlugin.groovy
+++ dslplugin/src/main/groovy/dslplugin/DslPlugin.groovy
@@ -46,7 +46,7 @@ class DslPlugin implements Plugin<Project> {
                 task.template = determineTemplateFileLocation(props, info.template)
                 task.omeXmlFiles = info.omeXmlFiles
                 task.outputPath = info.outputPath
-                task.outFile = info.outputFile
+                task.outFile = info.outFile
                 task.formatOutput = info.formatOutput
             }
 
